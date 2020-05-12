091019ec2fcb3ac502415573ba74ca5b3da34b0f
SpectrumVoIP <sswafford@spectrumvoip.com>
2020-05-12 14:51:17 -0500

    Added notes to includes/creds.sample.php

--- includes/creds.sample.php
+++ includes/creds.sample.php
@@ -1,7 +1,16 @@
 <?php
 
+# Copy this file to creds.php
+
+# Put your AWS token and key here
+# We recommend you create a key that only has access to Polly.
+# Don't use your AWS root key.
+
 $aws_token='xxxxx';
 $aws_key='yyyyy';
+
+# Get your key here: https://openweathermap.org/api
+# We just use the free subscription to get Weather by ZIP
 
 $openweathermapkey = 'zzzzz';
 
